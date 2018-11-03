9d371508343f9fe84edb64cc44445dfdbe27e512
apeckham <apeckham@gmail.com>
2018-11-02 22:20:16 -0700

    move the outer view into clojurescript

--- App.js
+++ App.js
@@ -5,18 +5,7 @@ import { Hi } from './shadow/demo.script';
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
         <Hi/>
-      </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- src/demo/script.cljs
+++ src/demo/script.cljs
@@ -3,6 +3,10 @@
             ["react-native" :as react-native]))
 
 (defn Hi []
-  (r/as-element [:> (.-Text react-native) 
+  (r/as-element [:> (.-View react-native)
+                 {:style {:flex 1
+                          :align-items 'center
+                          :justify-content 'center}}
+                  [:> (.-Text react-native) 
                  {:on-press #(js/alert "From clojurescript")}
-                  "clojurescript & shadow here"]))
+                  "clojurescript & shadow here!!~~~~~!!"]]))
